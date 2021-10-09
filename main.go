48869c8ed5349924ed81402e62fb7b84701a99af
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-10-09 18:26:41 +0545

    Posts endpoint created

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ type User struct {
 
 type Post struct {
 	ID 			primitive.ObjectID	`json:"_id,omitempty" bson:"_id,omitempty"`
+	Creator 	primitive.ObjectID	`json:"creator"`
 	Caption		string				`json:"caption"`
 	ImageURL	string				`json:"imageURL"`
 	Created		primitive.Timestamp	`json:"created,omitempty"`
@@ -58,13 +59,7 @@ func connectDb() {
 		log.Fatal("Couldn't connect to the database:\n", err)
 	}
 	log.Println("Database Connected!")
-	
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	fmt.Println(databases)
 	db = client.Database("officegram")
 }
 
@@ -76,21 +71,18 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 
     switch r.Method {
     case "GET":
-		cursor, err := db.Collection("users").Find(ctx, bson.M{})
+		userId, _ := primitive.ObjectIDFromHex(r.URL.Path[7:])
+		fmt.Println(userId)
+
+		filter := bson.D{{"_id", userId}}
+		var profile User
+
+		err := db.Collection("users").FindOne(ctx, filter).Decode(&profile)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		defer cursor.Close(ctx)
-
-		var users []User
-		for cursor.Next(ctx) {
-			var profile User
-			cursor.Decode(&profile)
-			users = append(users, profile)
-		}
-
-		json.NewEncoder(w).Encode(users)
+		json.NewEncoder(w).Encode(profile)
 
     case "POST":
 		var newUser User
@@ -110,6 +102,77 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+// Post Handlers
+func postHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+    switch r.Method {
+    case "GET":
+		postId, _ := primitive.ObjectIDFromHex(r.URL.Path[7:])
+		fmt.Println(postId)
+
+		filter := bson.D{{"_id", postId}}
+		var post Post
+
+		err := db.Collection("posts").FindOne(ctx, filter).Decode(&post)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(post)
+
+    case "POST":
+		var newPost Post
+		json.NewDecoder(r.Body).Decode(&newPost)
+
+		res, err := db.Collection("posts").InsertOne(ctx, newPost)
+        if err != nil {
+			log.Fatal(err)
+		}
+		
+        json.NewEncoder(w).Encode(res)
+		fmt.Println("Post Created")
+
+    default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(`{"message": "not found"}`))
+    }
+}
+
+// Users Post Handlers
+func userPostHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+    switch r.Method {
+    case "GET":
+		userId, _ := primitive.ObjectIDFromHex(r.URL.Path[13:])
+
+		filter := bson.D{{"creator", userId}}
+		var posts []Post
+
+		cursor, err := db.Collection("posts").Find(ctx, filter)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer cursor.Close(ctx)
+
+		for cursor.Next(ctx) {
+			var post Post
+			cursor.Decode(&post)
+			posts = append(posts, post)
+		}
+
+		json.NewEncoder(w).Encode(posts)
+
+    default:
+        w.WriteHeader(http.StatusNotFound)
+        w.Write([]byte(`{"message": "not found"}`))
+    }
+}
+
 
 
 func main() {
@@ -117,7 +180,9 @@ func main() {
 
 	connectDb()
 	
-	http.HandleFunc("/users", userHandler)
+	http.HandleFunc("/users/", userHandler)
+	http.HandleFunc("/posts/users/", userPostHandler)
+	http.HandleFunc("/posts/", postHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
